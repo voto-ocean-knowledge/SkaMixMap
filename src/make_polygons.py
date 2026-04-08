42a2a75f1af0f22e8ed5e21795fe77f7a8e20771
Callum Rollo <c.rollo@outlook.com>
2026-04-08 12:19:39 +0200

    three days of ftle

--- src/make_polygons.py
+++ src/make_polygons.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import xarray as xr
+import pandas as pd
+import datetime
+import requests
+import numpy as np
 import os
 import matplotlib.pyplot as plt
 import json
@@ -7,7 +11,7 @@ from shapely.geometry import Polygon
 import geopandas as gpd
 folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-path_to_emodnet = Path("/home/callum/Documents/datasets/bathy/emodet_2024/D5_2024.nc")
+path_to_emodnet = Path("/data/temp/D5_2024.nc")
 
 
 
@@ -48,69 +52,68 @@ def bathy_to_geojson(extent=[57.4, 59, 8, 12], depths=(100, 200), directory=Path
         with open(isobaths_dir/ (str(abs(depth)) + 'm.json'), 'w', encoding='utf-8') as f:
             json.dump(geo_json, f)
 
-color_dict = ['yellow', 'orange', 'red']
 
+def fetch_ftle():
+    response = requests.get('https://hyrax.iow.de/opendap/hyrax/dataset_00075/dataset_00075_data/file_0001.nc')
+    with open("/data/temp/ftle.nc", mode="wb") as file:
+        file.write(response.content)
 
+    
 def ftle_grad_to_polygons(directory):
-    ds = xr.open_dataset("/home/callum/Downloads/ftle_data.nc")
-    var_name = 'ftle'
-    min_time = ds.time.min()
-    lines = []
-    for i, threshold in enumerate([2e-5, 4e-5, 6e-5]):
-        variable = ds[var_name].loc[min_time]
-        var_copy = variable.copy()
-
-        var_copy = var_copy.fillna(0)
-        fig, ax = plt.subplots()
-        cs = ax.contour(ds.lon, ds.lat, var_copy, [threshold])
-        shapes = cs.allsegs[0]
-        for v in shapes:
-            if len(v) < 4:
-                # Trying to write less than 4 points to a polygon fails, skip these
-                continue
-            popup = f"{var_name} > {threshold} {str(min_time.values)[:13]}"
-            coords = [[point[0], point[1]] for point in v]
-
-            polygon = {
-                "geometry": {"type": "Polygon", "coordinates": [coords]},
-                "type": "Feature",
-                "properties": {
-                    "popupContent": popup,
-                    "color": color_dict[i],
-                },
-            }
-            lines.append(polygon)
-    poly_dict = {"type": "FeatureCollection", "features": lines}
-
     if not directory.exists():
         directory.mkdir(parents=True)
-    with open(directory /  f'{var_name}.json', 'w', encoding='utf-8') as f:
-        json.dump(poly_dict, f)
+    ftle_vars_dict = {
+        'temp_grad': {'var_name': 'temp_grad',
+                     'thresholds': [1e-4, 2e-4, 3e-4],
+                      'color_dict': ['#FFA0A0', '#FF5050', '#FF0000'],
+                      },
+        'salt_grad': {'var_name': 'salt_grad',
+                      'thresholds': [1e-4, 2e-4, 3e-4],
+                      'color_dict': ['#A0A0FF', '#5050FF', '#0000FF'],
+                      },
+        'ftle': {'var_name': 'ftle',
+                 'thresholds': [2e-5, 4e-5, 6e-5],
+                 'color_dict': ['#FFFFFF', '#808080', '#000000'],
+                 },
+    }
+    ds = xr.open_dataset("/data/temp/ftle.nc")
+    timesteps = pd.date_range("2026-01-01T14:00:00", "2027-01-03T14:00:00")
+    timesteps = timesteps[timesteps > np.datetime64(datetime.datetime.now())][:3]
+    for day, timestep in enumerate(timesteps):
+        lines = []
+        for var_name, var_dict in ftle_vars_dict.items():
+            for i, threshold in enumerate(var_dict['thresholds']):
+                variable = ds[var_name].loc[timestep]
+                var_copy = variable.copy()
+                var_copy = var_copy.fillna(0)
+                fig, ax = plt.subplots()
+                cs = ax.contour(ds.lon, ds.lat, var_copy, [threshold])
+                shapes = cs.allsegs[0]
+                for v in shapes:
+                    if len(v) < 4:
+                        # Trying to write less than 4 points to a polygon fails, skip these
+                        continue
+                    popup = f"{var_name} > {threshold}<br>{str(timestep)[:10]}"
+                    coords = [[point[0], point[1]] for point in v]
 
-    threshold = 0.0002
-    ds.temp_grad[-1, :, :].plot()
-    fig, ax = plt.subplots()
-    cs = ax.contour(ds.lon, ds.lat, ds.temp_grad[-1,:,:], [threshold])
-    shapes = cs.allsegs[0]
-    lines = []
-    print("extracting geometries")
-    for v in shapes:
-        if len(v) < 4:
-            # Trying to write less than 4 points to a polygon fails, skip these
-            continue
-        lines.append(Polygon(v))
+                    polygon = {
+                        "geometry": {"type": "Polygon", "coordinates": [coords]},
+                        "type": "Feature",
+                        "properties": {
+                            "popupContent": popup,
+                            "color": var_dict['color_dict'][i],
+                        },
+                    }
+                    lines.append(polygon)
+                plt.close('all')
+        poly_dict = {"type": "FeatureCollection", "features": lines}
 
-    # create geopandas dataframe and write it to json
-    line_gdf = gpd.GeoDataFrame(geometry=lines)
-    geo_json = line_gdf.to_json()
-    print("writing to file")
-    if not directory.exists():
-        directory.mkdir(parents=True)
-    with open(directory/  'ftle_temp.json', 'w', encoding='utf-8') as f:
-        json.dump(geo_json, f)
+        with open(directory /  f'ftle_{day}.json', 'w', encoding='utf-8') as f:
+            json.dump(poly_dict, f)
 
 
 if __name__ == '__main__':
     out_dir = data_dir = Path(folder) / 'static' / 'skamix2' / 'json'
+    #fetch_ftle()
     ftle_grad_to_polygons(out_dir / 'ftle')
     #bathy_to_geojson(directory=out_dir)
